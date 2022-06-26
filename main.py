c49988f7b698eccb7a0dbbf3fbfe2c3e80450351
Beth <71386265+colwilljenkins@users.noreply.github.com>
2022-06-26 21:29:59 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,34 +11,50 @@ turtle.shape(image)
 
 # Turning CSV into df
 df = pd.read_csv('50_states.csv')
+all_states = df.state.to_list()
 
 # game fundamentals
 game_is_on = True
 score = 0
+guessed_states = []
 
-# Prompting user input and saving to title case for search
-answer_state = screen.textinput(title='Guess the State', prompt="What's another states name?")
+# Game exit instructions
+exit_inst = turtle.Turtle()
+exit_inst.penup()
+exit_inst.hideturtle()
+exit_inst.goto(0, -260)
+exit_inst.write("Type 'Exit' to exit game and show missing states", align = 'center')
+
 
 while game_is_on:
-    answer = df[df['state'] == answer_state.title()]
-    if score == 50:
+    answer_state = screen.textinput(title=f'{score}/50 States', prompt="What's another states name?").title()
+    if answer_state == 'Exit':
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        missed_states = pd.DataFrame(missing_states)
+        missed_states.to_csv('States-to-learn.csv')
+        game_is_on = False
+    elif score == 50:
         over = turtle.Turtle()
         over.penup()
         over.hideturtle()
         over.write('YOU WIN', align = 'center', font = ('courier', 40, 'bold'))
         game_is_on = False
-    if len(answer) > 0:
-        y_cor = int(answer['y'])
-        x_cor = int(answer['x'])
-        state_title = turtle.Turtle()
-        state_title.penup()
-        state_title.hideturtle()
-        state_title.goto(x_cor, y_cor)
-        state_title.write(answer_state.title(), align = 'center')
-        score += 1
-        answer_state = screen.textinput(title=f'{score}/50 States', prompt="What's another states name?")
-    else:
-        answer_state = screen.textinput(title=f'{score}/50 States', prompt="Not correct\nWhat's another states name?")
+    elif answer_state in all_states:
+        if answer_state in guessed_states:
+            pass
+        else:
+            guessed_states.append(answer_state)
+            state_df = df[df['state'] == answer_state]
+            state_title = turtle.Turtle()
+            state_title.penup()
+            state_title.hideturtle()
+            state_title.goto(int(state_df.x), int(state_df.y))
+            state_title.write(answer_state.title(), align = 'center')
+            score += 1
+
 
 screen.exitonclick()
 
